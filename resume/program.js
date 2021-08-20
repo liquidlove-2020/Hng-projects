88d004ec7ab6974806773b04519397cf0ac0c184
Joseph Bassey <engruwehbassey2@gmail.com>
2021-08-20 15:22:07 +0100

    finished designing a version of my resume with html and bootstrap4 css

---
+++ resume/program.js
@@ -0,0 +1,48 @@
+
+    // PROGRAM TO WRITE NAME
+  alert("JOSEPH BASIONO BASSEY");
+
+    // SCROLL REVEAL SCRIPT
+        window.sr = ScrollReveal();
+        sr.reveal('.display', {
+            duration: 2000,
+            origin: 'left',
+            distance: '100px'
+        });
+        sr.reveal('.description', {
+            duration: 2000,
+            origin: 'left',
+            distance: '100px',
+            viewFactor: 0.2
+        });
+        sr.reveal('.competence', {
+            duration: 2000,
+            origin: 'left',
+            distance: '100px',
+            viewFactor: 0.2
+        });
+        sr.reveal('.contact', {
+            duration: 2000,
+            origin: 'left',
+            distance: '100px',
+            viewFactor: 0.2
+        });
+        sr.reveal('.experience', {
+            duration: 2000,
+            origin: 'right',
+            distance: '100px'
+        });
+        sr.reveal('.academics', {
+            duration: 2000,
+            origin: 'right',
+            distance: '100px',
+            viewFactor: 0.2
+        });
+        sr.reveal('.char-ref', {
+            duration: 2000,
+            origin: 'right',
+            distance: '100px',
+            viewFactor: 0.2
+        });
+        
+
